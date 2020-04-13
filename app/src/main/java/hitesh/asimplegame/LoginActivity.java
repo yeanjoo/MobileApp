b3279f226faf5d0cd746954798610bd88f10b16e
jooyena <reve.yeans2@gmail.com>
2020-04-13 13:23:05 +0900

    first

---
+++ app/src/main/java/hitesh/asimplegame/LoginActivity.java
@@ -0,0 +1,64 @@
+package hitesh.asimplegame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+    private Button SIGNUP;
+    private Button LOGIN;
+    private EditText ID;
+    private EditText PW;
+
+    private UserDBOpenHelper database;
+
+//DB값을 String으로 변환해서 담는 임시 변수들
+    private String id;
+    private String pw;
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        SIGNUP = (Button)findViewById(R.id.signup);
+        LOGIN = (Button)findViewById(R.id.login);
+        ID= (EditText)findViewById(R.id.username);
+        PW= (EditText)findViewById(R.id.password);
+        SIGNUP.setEnabled(true);
+        LOGIN.setEnabled(true);
+        SIGNUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(), SignUpActivity.class);//다음페이지
+                startActivity(intent);
+            }
+        });
+
+        LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                id = ID.getText().toString();
+                pw = PW.getText().toString();
+                check();
+            }
+        });
+    }
+    public void check(){
+        if(id.isEmpty()||pw.isEmpty()){
+            Toast myToast = Toast.makeText(getApplicationContext(), R.string.NULL_MESSAGE,Toast.LENGTH_SHORT);
+            myToast.show();
+        }
+        else if(database.isUser(id,pw)){
+            intent = new Intent(getApplicationContext(), QuestionActivity.class);//다음페이지
+            startActivity(intent);
+        }
+        else{//user가 존재하지 않는 경우
+            Toast myToast = Toast.makeText(getApplicationContext(), R.string.NOT_MATCH_MESSAGE,Toast.LENGTH_SHORT);
+            myToast.show();
+        }
+    }
+}
---
+++ app/src/main/java/hitesh/asimplegame/UserDBOpenHelper.java
@@ -0,0 +1,89 @@
+package hitesh.asimplegame;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDBOpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "USER";
+    //테이블 이름
+    private static final String TABLE_USER = "user";
+    private static final String TABLE_SCORE = "score"; //스코어 테이블을 따로 만들어 조인해주기
+    private static final String NAME = "name"; // user name
+    private static final String ID = "id"; // user id
+    private static final String PASSWORD = "password"; // user password
+    private static final String SCORE = "score";
+
+    private SQLiteDatabase database;
+
+    public UserDBOpenHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {//TABLE_USER(ID(PRI),PW,NAME); TABLE_SCORE(ID(PRI),SCORE)
+        database = db;
+        String sql_user = "CREATE TABLE IF NOT EXISTS " + TABLE_USER+ " ( "
+                + ID + " TEXT PRIMARY KEY, " + NAME + " TEXT, "
+                + PASSWORD + " TEXT)";
+
+        String sql_score = "CREATE TABLE IF NOT EXISTS " + TABLE_SCORE+ " ( "
+                + ID + " TEXT PRIMARY KEY, " + SCORE + " INT)";
+        db.execSQL(sql_user);
+        db.execSQL(sql_score);
+        // db.close();
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SCORE);
+        // Create tables again
+        onCreate(db);
+    }
+    public void addUser(User user){
+        ContentValues values = new ContentValues();
+        values.put(NAME,user.getNAME());
+        values.put(PASSWORD,user.getPW());
+        values.put(ID,user.getPW());
+        database.insert(TABLE_USER, null,values);
+    }
+    public void addScore(User user, int score){
+        ContentValues values = new ContentValues();
+        values.put(SCORE,score);
+        values.put(ID,user.getPW());
+        database.insert(TABLE_SCORE, null,values);
+    }
+    public User getUser(String ID){//ID에 해당되는 유저의 스코어를 포함한 모든 정보를 가져온다
+        User user = new User(); //중복아이디 막기... 이거 너무 귀찮으니까 나중에 하자
+
+        String selectQuery ="SELECT * FROM user INNER JOIN score ON user.ID=score.ID WHERE user.ID=ID";
+        //이너 조인, 일단 SQL
+        database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(selectQuery,null);
+
+        if (cursor.moveToFirst()) {
+            user.setID(cursor.getString(0));
+            user.setNAME(cursor.getString(1));
+            user.setPW(cursor.getString(2));
+            do {
+                user.ScoreUP(cursor.getInt(3));//score만 지속적으로 가져오기
+            } while (cursor.moveToNext());
+        }
+        return user;
+    }
+    public Boolean isUser(String id,String pw){
+        String checkQuery ="SELECT"+PASSWORD+"FROM"+TABLE_USER+"WHERE "+ID+"="+id;
+        if(checkQuery==pw) return true;
+        else return false;
+    }
+    //DB열고 닫기 메모리 누수 방지
+    public void openDB(){
+
+    }
+    public void closeDB(){
+
+    }
+}
